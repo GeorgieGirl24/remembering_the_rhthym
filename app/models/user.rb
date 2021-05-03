0b59bb4eaf2031126cbde96f47ee80305c2cacaf
George Soderholm <georgesoderholm@gmail.com>
2021-05-03 15:51:28 -0600

    Add validation of attributes for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # has_many :photos
+  
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
+  validates_uniqueness_of :email
 end
