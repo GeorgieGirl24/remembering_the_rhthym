ee10023d796d238d4281f6b73702f901acde0d98
George Soderholm <georgesoderholm@gmail.com>
2021-05-03 15:51:52 -0600

    Add validation of attributes for Concert

--- spec/models/concert_spec.rb
+++ spec/models/concert_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Concert, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :band_name }
+    it { should validate_presence_of :venue }
+    it { should validate_presence_of :concert_date }
+  end
+
+  describe 'relationships' do
+    # it { should have_many :photos }
+  end
 end
