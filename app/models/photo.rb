9d81ac10bf7f6b7dfbc834aa4750ee9a8b579ad0
George Soderholm <georgesoderholm@gmail.com>
2021-05-03 16:02:04 -0600

    Add validations and relationships to Photo

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :user
   belongs_to :concert
+
+  validates :name, presence: true
+  validates :concert_date, presence: true
+  validates :loved, inclusion: [true, false]
+  validates :liked, inclusion: [true, false]
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Photo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :concert_date }
+    # it { should validate_inclusion_of(:loved).in_array([true, false]) }
+    # it { should validate_inclusion_of(:liked).in_array([true, false]) }
+  end
+
+
+  describe 'relationships' do
+    it { should belong_to :user }
+    it { should belong_to :concert }
+  end
 end
