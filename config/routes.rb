02323f61f7ec96e86956e0cf2bef1791f0b066c3
George Soderholm <georgesoderholm@gmail.com>
2021-05-03 21:10:27 -0600

    Update the routes for the User to navigate to the correction path

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,22 @@
 Rails.application.routes.draw do
+  resources :welcome, :path => '/', only: :index
   resources :photo_tags
   resources :tags
   resources :photos
   resources :concerts
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :create, :update]
+
+  scope controller: :users do
+    get 'registration' => :new, as: :registration
+    patch '/user/:id' => :update
+    get '/profile' => :show
+    get '/profile/edit' => :edit
+    get '/profile/edit_password' => :edit_password
+  end
+
+  scope controller: :sessions do
+    get '/login' => :new
+    post '/login' => :login
+    get '/logout' => :logout
+  end
 end
