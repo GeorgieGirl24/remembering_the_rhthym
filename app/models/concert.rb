3c863d9bd55c3f27d662e669bf50de04e9429781
George Soderholm <georgesoderholm@gmail.com>
2021-05-05 10:25:51 -0600

    Add relationships to User through UserConcert

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,5 +1,7 @@
 class Concert < ApplicationRecord
   has_many :photos, dependent: :destroy
+  has_many :user_concerts, dependent: :destroy
+  has_many :users, through: :user_concerts, dependent: :destroy
 
   validates :band_name, presence: true
   validates :venue, presence: true
