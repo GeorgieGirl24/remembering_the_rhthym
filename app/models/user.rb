9264718c442d6c4205927e0ff8b4745bf123315f
George Soderholm <georgesoderholm@gmail.com>
2021-05-05 10:25:15 -0600

    Add relationships to Concert through UserConcert

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_one_attached :avatar
   has_many :photos, dependent: :destroy
+  has_many :user_concerts, dependent: :destroy
+  has_many :concerts, through: :user_concerts, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :email }
-    it { should validate_presence_of :password_digest }
   end
 
   describe 'relationships' do
     it { should have_many :photos }
+    it { should have_many :user_concerts }
+    it { should have_many(:concerts).through(:user_concerts) }
   end
 end
