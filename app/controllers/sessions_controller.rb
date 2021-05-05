90d111c464b3ae4f1f5c84719269433a254d5fc6
George Soderholm <georgesoderholm@gmail.com>
2021-05-05 14:22:14 -0600

    Remove pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,10 @@ class SessionsController < ApplicationController
 
   def login
     user = User.find_by(email: params[:email])
-    binding.pry
     if user && user.authenticate(params[:password])
       login_redirect(user)
     else
-      flash[:notice] = 'Your email or password was incorrect!'
+      flash.now[:notice] = 'Your email or password was incorrect!'
       render :new
     end
   end
